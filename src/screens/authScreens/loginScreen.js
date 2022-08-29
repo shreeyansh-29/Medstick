f7fffd26f1c5ea1ade1ddb68722f495436b40083
shreeyansh29 <shreeyansh.singh@nineleaps.com>
2022-08-29 12:45:01 +0530

    inital commit

---
+++ src/screens/authScreens/loginScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const loginScreen = () => {
+  return (
+    <View>
+      <Text>loginScreen</Text>
+    </View>
+  );
+};
+
+export default loginScreen;
